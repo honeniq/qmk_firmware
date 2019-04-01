251eb2d072889d3d435d6786b3460a05075a16bc
honeniq <honeniq@gmail.com>
2019-04-01 23:01:02 +0900

    Add retro tapping

--- keyboards/lily58/keymaps/honeniq/keymap.c
+++ keyboards/lily58/keymaps/honeniq/keymap.c
@@ -23,6 +23,8 @@ extern uint8_t is_master;
 #define _RAISE 2
 #define _ADJUST 3
 
+#define RETRO_TAPPING
+
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   LOWER,
@@ -55,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   KC_LCTRL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
-               LALT_T(EISU), KC_LGUI, LOWER, KC_SPC,   KC_ENT,   RAISE,   KC_BSPC,    KANA \
+               LALT_T(EISU), KC_LGUI, LOWER, LSFT_T(KC_SPC),   KC_ENT,   RAISE,   KC_BSPC,    KANA \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
