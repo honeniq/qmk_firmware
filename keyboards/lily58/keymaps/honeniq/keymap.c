6613c12b436a83c736ec88c76019487643102171
Hiroaki Usami <hiroaki.usami@ponos.co.jp>
2019-04-02 16:39:30 +0900

    Fix disable LALT_T

--- keyboards/lily58/keymaps/honeniq/keymap.c
+++ keyboards/lily58/keymaps/honeniq/keymap.c
@@ -47,8 +47,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |Enter |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | EISU  | LGUI |LOWER | /Space /       \Enter \  |RAISE |BackSP| KANA |
- *                   | (ALT) |      |      |/      /         \      \ |      |      | (ALT)|
+ *                    | LALT  | LGUI |LOWER | /Space /       \Enter \  |RAISE |BackSP| RALT |
+ *                   |       |      |      |/      /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   KC_LCTRL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
-               LALT_T(EISU), KC_LGUI, LOWER, LSFT_T(KC_SPC),   KC_ENT,   RAISE,   KC_BSPC,    KANA \
+                   KC_LALT,  KC_LGUI, LOWER, LSFT_T(KC_SPC),   KC_ENT,   RAISE,   KC_BSPC,   KC_RALT \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -69,8 +69,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
  * |      |      |      |      |      |      |-------|    |-------|      |   _  |   +  |   {  |   }  |   |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | EISU  | LGUI |LOWER | /Space /       \Enter \  |RAISE |BackSP| KANA |
- *                   | (ALT) |      |      |/      /         \      \ |      |      | (ALT)|
+ *                    | LALT  | LGUI |LOWER | /Space /       \Enter \  |RAISE |BackSP| RALT |
+ *                   |       |      |      |/      /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 [_LOWER] = LAYOUT( \
@@ -90,8 +90,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|   {   |    |    }  |------+------+------+------+------+------|
  * |  F7  |  F8  |  F9  | F10  | F11  | F12  |-------|    |-------|   +  |   -  |   =  |   [  |   ]  |   \  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | EISU  | LGUI |LOWER | /Space /       \Enter \  |RAISE |BackSP| KANA |
- *                   | (ALT) |      |      |/      /         \      \ |      |      | (ALT)|
+ *                    | LALT  | LGUI |LOWER | /Space /       \Enter \  |RAISE |BackSP| RALT |
+ *                   |       |      |      |/      /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 
